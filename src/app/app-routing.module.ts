611587be93cf69e04f337bbabc9a90d6c19f7b92
JPCabral04 <jpqcabral04@gmail.com>
2025-02-21 10:26:21 -0300

    Add routing configuration and update dependencies. Closes #3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { RegistryComponent } from './pages/registry/registry.component';
+import { StockComponent } from './pages/stock/stock.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: LoginComponent },
+  { path: "home", component: HomeComponent },
+  { path: "profile", component: ProfileComponent },
+  { path: "registry", component: RegistryComponent },
+  { path: "stock", component: StockComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
