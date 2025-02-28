1a6c4dc227ed1b2e34b36a839c2748f55c488906
JPCabral04 <jpqcabral04@gmail.com>
2025-02-28 18:56:58 -0300

    feat(users): Implement user filtering by attributes in user list

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -12,21 +12,36 @@ export class UsersComponent implements OnInit {
 
   db = inject(DatabaseService);
   users?: IUser[];
+  filteredUsers?: IUser[];
 
   ngOnInit(): void {
     this.db.getCollection<IUser>('users').subscribe(users => {
 
       if (users) {
         this.users = users;
-        console.log(this.users);
       } else {
         console.log('Não foi possível encontrar os usuários');
       }
-    }
 
+      this.filteredUsers = users;
+    }
     )
   }
 
+  filterUsers(event: Event): void {
+    const input = (event.target as HTMLInputElement).value.toLowerCase();
+
+    if (input.trim() === '') {
+      this.filteredUsers = this.users;
+    }
+
+    this.filteredUsers = this.users?.filter(user =>
+      user.nome?.toLowerCase().includes(input) ||
+      user.codigo?.toLowerCase().includes(input) ||
+      user.email?.toLowerCase().includes(input) ||
+      user.tipoUsuario?.toLowerCase().includes(input)
+    );
+  }
 
 
 }
