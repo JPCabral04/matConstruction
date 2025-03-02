609a6ccf0b862220b9b3a4737ed0e51b91c5986f
JPCabral04 <jpqcabral04@gmail.com>
2025-03-01 21:10:50 -0300

    feat(stock): Implement stock table with dynamic data fetching and custom header/body

--- src/app/pages/stock/stock.component.ts
+++ src/app/pages/stock/stock.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IStock } from '../../shared/interfaces/stock.interface';
+import { DatabaseService } from '../../shared/services/database.service';
 
 @Component({
   selector: 'app-stock',
   templateUrl: './stock.component.html',
   styleUrl: './stock.component.scss'
 })
-export class StockComponent {
+export class StockComponent implements OnInit {
+
+  stockItems: IStock[] = [];
+
+  constructor(private db: DatabaseService) { };
+
+  ngOnInit(): void {
+    this.db.getCollection<IStock>('estoques').subscribe(items => {
+      if (items) {
+        this.stockItems = items;
+      }
+    })
+  }
 
 }
